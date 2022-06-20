0c55b30fd3e409e7a96f2526e068d40be5dd94ec
a-ghanem <aghanem@vt.edu>
2022-06-20 09:27:33 +0000

    Deploying to gh-pages from  @ 315527e8afc1794af41f8a336e6dffd06a0af31e 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.uio.ifi.clearinghouse"},{"l":"no.uio.ifi.clearinghouse.model"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"no.uio.ifi.clearinghouse"},{"l":"no.uio.ifi.clearinghouse.model"}];updateSearchResults();
